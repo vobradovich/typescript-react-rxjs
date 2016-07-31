9696bb0942a4e77354b9db1daaebc301beee4b71
Vadim Obradovich <vadik.on@gmail.com>
2016-07-31 14:13:59 +0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+/// <reference path="../typings/index.d.ts" />
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(<div />, document.getElementById('root'));
