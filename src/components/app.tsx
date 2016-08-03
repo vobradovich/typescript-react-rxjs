83583838c338d9bbf8c38126613ee791a405f0b2
Vadim Obradovich <vadik.on@gmail.com>
2016-08-03 23:22:18 +0300

    Add components

---
+++ src/components/app.tsx
@@ -0,0 +1,8 @@
+/// <reference path="../../typings/index.d.ts" />
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export const App = (props) => (
+    <div {...props}></div>
+);
