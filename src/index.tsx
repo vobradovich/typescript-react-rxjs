4cf2351495dedbda2272563e363fe822a5f2c2f7
Vadim Obradovich <vadik.on@gmail.com>
2016-08-03 23:22:41 +0300

    Fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as Rx from 'rxjs';
+import { App } from './components/app.tsx';
 
 var source = Rx.Observable.timer(1000, 1000);
 
@@ -11,5 +12,5 @@ var socket = Rx.Observable.webSocket("ws://echo.websocket.org/");
 source.subscribe(socket);
 
 socket.subscribe(o => {
-    ReactDOM.render(<div>{JSON.stringify(o) }</div>, document.getElementById('root'));
-});
+    ReactDOM.render(<App>{JSON.stringify(o) }</App>, document.getElementById('root'));
+}, e => { console.log(e) }, () => console.log("Complete"));
